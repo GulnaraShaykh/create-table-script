6d6fe9baf5efd1dc7019069c1a4c1ad3111f4c43
gulnarashaykhutdinova <gu_code@mail.ru>
2024-08-25 06:27:18 +0300

    initial

---
+++ src/main/java/com/example/createtablescript/CreateTableScriptApplication.java
@@ -0,0 +1,13 @@
+package com.example.createtablescript;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateTableScriptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreateTableScriptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/createtablescript/CreateTableScriptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.createtablescript;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateTableScriptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
